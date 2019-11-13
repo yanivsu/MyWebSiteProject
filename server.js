756209bcb7104d57c22406d0fde5f3b90d9e72f0
yanivsu <32729242+yanivsu@users.noreply.github.com>
2019-11-14 01:19:26 +0200

    first

--- server.js
+++ server.js
@@ -7,9 +7,12 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/MyWebSiteProject'));
 
-app.get('/*', function(req,res) {
+app.get('/', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/MyWebSiteProject/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080,function (err) {
+  if(err) throw err
+  console.log('Server is Listen to 8080');
+});
