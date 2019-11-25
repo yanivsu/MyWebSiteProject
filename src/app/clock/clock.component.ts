f262fee8117044a384360039fd11987c65cf1b7a
yanivsu <32729242+yanivsu@users.noreply.github.com>
2019-11-25 12:55:03 +0200

    :)

---
+++ src/app/clock/clock.component.ts
@@ -0,0 +1,41 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-clock',
+  templateUrl: './clock.component.html',
+  styleUrls: ['./clock.component.css']
+})
+export class ClockComponent implements OnInit, OnDestroy {
+
+  hours: string;
+  minutes: string;
+  seconds: string;
+  private timerId = null;
+
+  ngOnInit() {
+    this.setCurrentTime();
+    this.timerId = this.updateTime();
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.timerId);
+  }
+
+  private setCurrentTime() {
+    const time = new Date(Date.now());
+    this.hours = this.leftPadZero(time.getHours());
+    this.minutes = this.leftPadZero(time.getMinutes());
+    this.seconds = this.leftPadZero(time.getSeconds());
+  }
+
+  private updateTime() {
+    setInterval(() => {
+      this.setCurrentTime();
+    }, 1000);
+  }
+
+  private leftPadZero(value: number) {
+    return value < 10 ? `0${value}` : value.toString();
+  }
+
+}
